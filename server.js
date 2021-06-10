fb06924126b74b3b057f2d42ede5a22dcc167202
yuriranieri <yurifranieri@gmail.com>
2021-06-10 20:08:34 -0300

    iniciando projeto

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+
+const port = 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log("Rodando...")
+});
