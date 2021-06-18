cdabeb0ff5547a04650e4809ebad6cc459cbbaa9
yuriranieri <yurifranieri@gmail.com>
2021-06-18 19:41:44 -0300

    adicionando busca no ranking em ordem decrescente

--- controller/test.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const User = require('../models/user');
-
-router.get('/', (req, res) => {
-    User.findAll().then(user => res.json(user))
-});
-
-module.exports = router;
